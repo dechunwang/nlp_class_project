d6c18def02b326aa0ebbe591452e1341a0aef490
BELZHANG <zhangchenxi830@gmail.com>
2019-03-11 14:51:42 -0400

    model class initial

---
+++ model/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.utils.data as Data
+import torchvision
+
+class ROC_CNN(nn.Module):
+    def __init__(self):
+        super(ROC_CNN).__init__()
+        self.conv1 = nn.Conv1d()
+        pass
+
+
+
+    def forward(self, *input):
+        pass
