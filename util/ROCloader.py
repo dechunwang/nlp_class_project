975264deb9e9126466b78939f6e7917a12dc6a5b
dechunwang <dechun94@gmail.com>
2019-03-13 14:08:39 -0400

    Add pickel and padding

--- util/ROCloader.py
+++ util/ROCloader.py
@@ -7,21 +7,31 @@ import pandas as pd
 import os
 from nltk.tokenize import word_tokenize
 from collections import defaultdict
+import pickle
 
 import string
 
 
 class ROCloader(Dataset):
-    def __init__(self, directory, mode):
-        self.pre_max = 0
-        self.suf_max = 0
+    def __init__(self, directory, mode,prefix_length, suffix_length):
+
+        self.prefix_length = prefix_length
+        self.suffix_length = suffix_length
         self.storys = []
 
+
+        w_2_c_path ='../dataset/wiki-news-300d-1M.vec'
         # loading pretrained 300d word2vec
-        word2vec_wiki_300 = KeyedVectors.load_word2vec_format('../dataset/wiki-news-300d-1M.vec', binary=False)
+        if os.path.isfile(os.path.splitext(w_2_c_path)[0]+'.cache'):
+            with open(os.path.splitext(w_2_c_path)[0] + '.cache', 'rb') as f:
+                word2vec_wiki_300 = pickle.load(f)
+        else:
+            word2vec_wiki_300 = KeyedVectors.load_word2vec_format(w_2_c_path, binary=False)
+            with open(os.path.splitext(w_2_c_path)[0]+'.cache', 'wb') as f:
+                pickle.dump(word2vec_wiki_300,f, pickle.HIGHEST_PROTOCOL)
 
         if mode == "test":
-            file_name = 'cloze_test_test__spring2016 - clolist(rows[1][1:5])ze_test_ALL_test.csv'
+            file_name = 'cloze_test_test__spring2016 - cloze_test_ALL_test.csv'
         elif mode == "val":
             file_name = 'cloze_test_val__spring2016 - cloze_test_ALL_val.csv'
 
@@ -39,33 +49,28 @@ class ROCloader(Dataset):
             # suffix_2 = [word for word in word_tokenize(rows[1][6]) if word not in string.punctuation]
 
             prefix = [word for word in word_tokenize(" ".join(list(rows[1][1:5]))) if word not in string.punctuation]
-            if len(prefix) > self.pre_max:
-                self.pre_max = len(prefix)
 
             for index, token in enumerate(prefix):
                 if token not in word2vec_wiki_300.vocab:
-                    prefix[index] = np.random.rand(300)
+                    prefix[index] = np.zeros(300)
                 else:
                     prefix[index] = word2vec_wiki_300[token]
-                    print(type(prefix[index]), prefix[index].shape)
+                    # print(type(prefix[index]), prefix[index].shape)
 
             suffix_1 = [word for word in word_tokenize(rows[1][5]) if word not in string.punctuation]
-            if len(suffix_1) > self.suf_max:
-                self.suf_max = len(suffix_1)
 
             for index, token in enumerate(suffix_1):
                 if token not in word2vec_wiki_300.vocab:
-                    suffix_1[index] = np.random.rand(300)
+                    suffix_1[index] = np.zeros(300)
                 else:
                     suffix_1[index] = word2vec_wiki_300[token]
 
             suffix_2 = [word for word in word_tokenize(rows[1][6]) if word not in string.punctuation]
-            if len(suffix_2) > self.suf_max:
-                self.suf_max = len(suffix_2)
+
 
             for index, token in enumerate(suffix_2):
                 if token not in word2vec_wiki_300.vocab:
-                    suffix_2[index] = np.random.rand(300)
+                    suffix_2[index] = np.zeros(300)
                 else:
                     suffix_2[index] = word2vec_wiki_300[token]
 
@@ -99,21 +104,21 @@ class ROCloader(Dataset):
         return len(self.storys)
 
     def __getitem__(self, idx):
-        prefix = self.storys[idx]['prefix']
-        for x in range (len(prefix), self.pre_max):
-            padding = np.zeros(300)
-            prefix.append(padding)
+        prefix = np.array( self.storys[idx]['prefix'])
 
-        sufix = self.storys[idx]['suffix']
-        for x in range(len(sufix), self.suf_max):
-            padding = np.zeros(300)
-            sufix.append(padding)
+        prefix_pad = np.zeros((self.prefix_length,300))
+        prefix_pad[:prefix.shape[0],:]=prefix
+
+        sufix =  np.array(self.storys[idx]['suffix'])
+
+        sufix_pad = np.zeros((self.suffix_length, 300))
+        sufix_pad[:sufix.shape[0], :] = sufix
 
         label = self.storys[idx]['gt_class']
-        return prefix, sufix, label
+        return prefix_pad.transpose(), sufix_pad.transpose(), label
 
 
 if __name__ == '__main__':
-    data= ROCloader('../dataset/','val')
+    data= ROCloader('../dataset/','val',100,20)
     prifix, sufix, label = data[8]
-    print(len(prifix))
+    print(prifix)
