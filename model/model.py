1d1c55f99d68c4294c166add4dd69c5ef9454410
dechunwang <dechun94@gmail.com>
2019-03-13 14:55:55 -0400

    Add pickel and padding

--- model/model.py
+++ model/model.py
@@ -1,24 +1,79 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.data as Data
 import torchvision
+
+
+class Conv1d (nn.Module):
+    def __init__(self, input_channels, output_channels, kernel_size, stride,same_padding, bias=True, relu=True):
+        super(Conv1d).__init__()
+        padding = int((kernel_size - 1) / 2) if same_padding else 0
+        self.conv = nn.Conv1d(input_channels,output_channels,kernel_size,stride,padding,bias=bias)
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, input):
+        x =self.conv(input)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
 
 class ROC_CNN(nn.Module):
     def __init__(self, input_channels):
         super(ROC_CNN).__init__()
         self.conv1_prefix = nn.Sequential(
-            nn.Conv1d(in_channels=input_channels, out_channels=128, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
+            Conv1d(input_channels,256,3,1,same_padding=True,bias=True,relu=True),
+            Conv1d(256, 256, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(256, 256, 3, 1, same_padding=True, bias=True, relu=True),
         )
+
+        self.conv2_prefix = nn.Sequential(
+            Conv1d(256, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
+        )
+
+        self.conv3_prefix = nn.Sequential(
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
+        )
+
         self.conv1_sufix = nn.Sequential(
-            nn.Conv1d(input_channels=input_channels, out_channels=128, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
+            Conv1d(input_channels,256,3,1,same_padding=True,bias=True,relu=True),
+            Conv1d(256, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(256, 256, 3, 1, same_padding=True, bias=True, relu=True),
+            )
+        self.conv2_sufix = nn.Sequential(
+            Conv1d(256, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
+            Conv1d(512, 512, 3, 1, same_padding=True, bias=True, relu=True),
         )
-        pass
+
+
+        self.linear_1 = nn.Linear(8704,4096,True)
+        self.linear_2 = nn.Linear(4096, 2, True)
 
 
 
-    def forward(self, *input):
-        pass
+    def forward(self, prefix,suffix,label):
+
+        conv1_prefix = F.max_pool1d(self.conv1_prefix(prefix))
+        conv2_prefix = F.max_pool1d(self.conv2_prefix(conv1_prefix))
+        conv3_prefix = F.max_pool1d(self.conv3_prefix(conv2_prefix))
+
+        conv1_suffix = F.max_pool1d(self.conv1_sufix(suffix))
+        conv2_suffix = F.max_pool1d(self.conv2_sufix(conv1_suffix))
+
+        fuse_ps = torch.cat(conv3_prefix,conv2_suffix,dim=0)
+
+        linear_1 = self.linear_1(fuse_ps)
+        linear_2 = self.linear_2(linear_1)
+        if self.training:
+
+            return F.cross_entropy(linear_2,label)
+
+        else:
+            return F.softmax(linear_2)
+
+
